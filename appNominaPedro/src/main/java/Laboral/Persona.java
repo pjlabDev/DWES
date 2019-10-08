94af5267b3e9ee8daeaf61bf1dfd3a9c649ba325
PedrassoxD <pedroperlab@gmail.com>
2019-10-08 15:35:36 +0200

    Nueva revision

---
+++ appNominaPedro/src/main/java/Laboral/Persona.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package Laboral;
+
+/**
+ * @author pedro
+ *
+ */
+public class Persona {
+	
+	//Atributos
+	
+	String nombre;
+	String dni;
+	char sexo;
+	
+	/**
+	 * @param nombre
+	 * @param dni
+	 * @param sexo
+	 */
+	
+	public Persona(String nombre, String dni, char sexo) {
+		
+		this.nombre = nombre;
+		this.dni = dni;
+		this.sexo =  sexo;
+		
+	}
+	
+	public Persona(String nombre, char sexo) {
+		
+		this.nombre = nombre;
+		this.sexo = sexo;
+		
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String imprime() {
+		
+		return nombre + " " + dni;
+		
+	}
+	
+	
+
+
+}
---
+++ appNominaPedro/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Página principal</h2>
+
+	<button onclick="location.href='ServletPedro.java'">Mostrar Empleados BBDD</button>
+	
+</body>
+</html>
