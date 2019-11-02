d2b4cef1c8e0d963c24f6aed242d328579bae9dc
PedrassoxD <pedroperlab@gmail.com>
2019-11-02 21:59:00 +0100

    Nueva revision

---
+++ empresaSpring/src/main/java/com/empresa/spring/EmpresaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaSpringApplication.class, args);
+	}
+
+}
